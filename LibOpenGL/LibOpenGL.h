a3360ef6fcb0b888a87729c4d3b3caa95e84c256
btj <btj@meitu.com>
2022-02-15 14:25:38 +0800

    libOpenGL

---
+++ LibOpenGL/LibOpenGL.h
@@ -0,0 +1,12 @@
+//
+//  LibOpenGL.h
+//  LibOpenGL
+//
+//  Created by meitu on 2022/2/15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibOpenGL : NSObject
+
+@end
---
+++ LibOpenGL/LibOpenGL.m
@@ -0,0 +1,14 @@
+//
+//  LibOpenGL.m
+//  LibOpenGL
+//
+//  Created by meitu on 2022/2/15.
+//
+
+#import "LibOpenGL.h"
+
+@implementation LibOpenGL
++(void)show{
+    NSLog(@"HelloWorld");
+}
+@end
